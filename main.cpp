8198a044d561f97036f065995c0a89939687d896
s5032765 <91558903+s5032765@users.noreply.github.com>
2021-12-08 17:57:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+using namespace std;
+
+float max(float lista[],int dim){
+	float max=lista[0];
+	for(int i=1; i<dim; i++){
+		if(lista[i]>max)
+			max=lista[i];
+	}
+	return max;
+}
 
 int main(){
-  return 0;
+	int dim;
+	cin>>dim;
+	float numeri[dim];
+	for(int i=0; i<dim; i++){
+		cin>>numeri[i];
+	}
+	cout<<max(numeri,dim);
+	return 0;
 }
